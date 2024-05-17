6e7e4ba534c077e318786adce14c097186992cb3
cvzi <cuzi@openmail.cc>
2024-05-17 11:24:25 +0200

    Initital commit

---
+++ tests/test_a_test.py
@@ -0,0 +1,5 @@
+from testutils import a_test_helper_function
+
+
+def test_my_test():
+    print(a_test_helper_function("This is a test"))
---
+++ utils/testutils.py
@@ -0,0 +1,5 @@
+import largedict
+
+
+def a_test_helper_function(s):
+    return s + " helper"
